9055a5c34942dd96570f788eeda4110b3795a5f8
mat <admin@c052068.wlan.net.ed.ac.uk>
2017-12-15 16:11:00 +0000

    DelayEffectBaseClass Template added

---
+++ BaseClasses/DigitalEffectBase.cpp
@@ -0,0 +1,13 @@
+//
+//  DigitalEffectBase.cpp
+//  AudioEffectsSuite
+//
+//  Created by admin on 12/12/2017.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#include "DigitalEffectBase.hpp"
+
+DigitalEffectBase::DigitalEffectBase () {}
+DigitalEffectBase::~DigitalEffectBase() {}
+
---
+++ BaseClasses/DigitalEffectBase.hpp
@@ -0,0 +1,99 @@
+//
+//  DigitalEffectBase.hpp
+//  AudioEffectsSuite
+//
+//  Created by admin on 12/12/2017.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#ifndef DigitalEffectBase_hpp
+#define DigitalEffectBase_hpp
+
+#include <stdio.h>
+
+class DigitalEffectBase;
+//==============================================================================
+/**
+	A Base class for delay based digital effects. Provides the basic methods
+	that are shared amongst Flanger, Delay, Chorus and Phaser
+ 
+ @see DigitalEffectBase
+ */
+class DigitalEffectBase
+{
+public:
+	/** Destructor. */
+	virtual ~DigitalEffectBase();
+	
+	//==============================================================================
+	/** Returns the names of all the available output channels on this device.
+	 To find out which of these are currently in use, call getActiveOutputChannels().
+	 */
+	virtual int getOutputNumber() = 0;
+	
+	/** Returns the names of all the available input channels on this device.
+	 To find out which of these are currently in use, call getActiveInputChannels().
+	 */
+	virtual int getInputNumber() = 0;
+	
+	//==============================================================================
+	/** Returns the delay buffer size
+	 */
+	virtual int getCurrentBufferSizeSamples() = 0;
+	
+	/** Returns the sample rate
+	 */
+	virtual double getCurrentSampleRate() = 0;
+	
+	/** Returns the current physical bit-depth.
+	 */
+	virtual int getCurrentBitDepth() = 0;
+	
+	//==============================================================================
+	
+	/** Tries to open the device ready to play.
+	 
+	 @param inputChannelNum         number of input channels to effect
+	 @param outputChannelNum        number of output channels from effect
+	 @param sampleRate				the sample rate to try to use
+	 @param bufferSizeSamples		the size of delay buffer to be used
+	 
+	 @returns   an error description if there's a problem, or sets the internal delay buffer
+	 
+	 */
+	
+	virtual void setDelayBuffer(int inputChannelNum,int outputChannelNum,int sampleRate,int bufferSizeSamples) = 0;
+	//==============================================================================
+	
+		//setMaxBufferSize, this should be informed by what kind off effect will be implemented
+	
+	//==============================================================================
+	/** Sets the internal lagrange interpolation table. Ideally it should be shared
+	 amongst all
+	 
+	 @param interpolationOrder	order of interpolation to be used
+	 @param alphaResolution      the number of discrete alpha values that are represented
+	 
+		@returns    an error description if there's a problem, or sets the interpolation lookup table
+	 
+	 
+	 */
+	virtual void setInterpolationTable(int interpolationOrder, int alphaResolution) = 0;
+	
+private:	//Methods
+	
+protected: // member variable
+	//==============================================================================
+	/** Constructor. */
+	DigitalEffectBase();
+	
+	//==============================================================================
+	/** Table of interpolation values as a 2D array indexed by
+		interpolationTable[pointIndex][alphaIndex]
+	 */
+	double** interpolationTable = 0;
+};
+
+
+
+#endif /* DigitalEffectBase_hpp */
