a0ea2e4b909be1a164ccf6f1ae4f46ddfe114e6a
joeyhook <joeyhook@gmail.com>
2017-12-19 17:49:36 +0000

    test commit

--- joey/Effect Suite/Effect Suite/main.cpp
+++ joey/Effect Suite/Effect Suite/main.cpp
@@ -6,10 +6,21 @@
 //  Copyright © 2017 Joseph Hook. All rights reserved.
 //
 
+#include <CoreFoundation/CoreFoundation.h>
 #include <iostream>
+#include "../../../AudioIOClasses/AudioPlayer.cpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, const char * argv[]){
+    //==========================================================================
+    // This only works if music is in the working diretory of the execution file
+    // This directory cannot be accessed absolutely but if is in a container
+    // Drop a file in here or change the destination of your xcode build
+    // in the File -> Workspace settings... Workspace relative location
+    //==========================================================================
+    printf("Path relative to the working directory is: %s\n", argv[0]);
+    
+    playAudio("/Users/joeyhook/Desktop/nes drums/effects/asskick.wav");
+    
+    return EXIT_SUCCESS;
 }
+
