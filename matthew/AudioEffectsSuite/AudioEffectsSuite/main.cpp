1b7a15508bcb67aa586afd0417198d7fc91d2d91
Matthew <mhamilton942@gmail.com>
2018-01-10 16:08:52 +0000

    white noise test

--- matthew/AudioEffectsSuite/AudioEffectsSuite/main.cpp
+++ matthew/AudioEffectsSuite/AudioEffectsSuite/main.cpp
@@ -16,18 +16,28 @@
 #include "../../../AudioIOClasses/AudioPlayer.cpp"
 #include "../../../FilterEffects/SimpleLPF.cpp"
 
+double whiteNoise()
+{
+	const double lo = -1.;
+	const double hi =  1.;
+	return lo + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(hi-lo)));
+}
+
 int main(int argc, const char * argv[])
 {
 	//==============================================================================
 	//	printf("Path relative to the working directory is: %s\n", argv[0]);
 	SimpleLPF filterLeft(4), filterRight(4);
 	
+	double hi = 1., lo = -1.;
+	srand (static_cast <unsigned> (time(0)));
+	float r3 = lo + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(hi-lo)));
 	// Delay class initiliases with a delay value in samples.
 	SimpleDelay delayLeft(8000);
 	SimpleDelay delayRight(800);
 	SimpleFlanger flangerLeft;
 	SimpleFlanger flangerRight;
-	const char ifile[] = "/Users/admin/Downloads/Space Lion.wav";
+	const char ifile[] = "/Users/admin/Documents/Masters/PBMMI/Audio_Examples/GuitarStrum_12s_MN.wav";
 	//	"/Users/admin/Documents/Masters/PBMMI/Audio_Examples/GuitarStrum_12s_MN.wav"
 	//	"/Users/admin/Downloads/Space Lion.wav"
 	//	"/Users/admin/Documents/Masters/PBMMI/Audio_Examples/trigen.wav"
@@ -51,17 +61,17 @@ int main(int argc, const char * argv[])
 	
 	const double flangeDepth = sampleRate*0.001;
 	flangerLeft.setEffectParams(1, flangeDepth*2, .15);
-	flangerRight.setEffectParams(1, flangeDepth, .15);
+	flangerRight.setEffectParams(1, flangeDepth, .215);
 	//==============================================================================
 	for (int i = 0; i<numOfFrames;i++)
 //		for (int i = 0; i<10;i++)
 	{
-		stereoOut[0][i] = filterLeft.applyFilter(stereoIn[0][i]);
-		stereoOut[1][i] = filterRight.applyFilter(stereoIn[1][i]);
+		stereoOut[0][i] = filterLeft.applyFilter(whiteNoise());
+		stereoOut[1][i] = filterRight.applyFilter(whiteNoise());
 		double freq = .01 +double(i)/double(4*numOfFrames);
 		
-		filterLeft.changeChebyICoefficients(freq, false, .1);
-		filterRight.changeChebyICoefficients(freq, false, .1);
+		filterLeft.changeChebyICoefficients(freq, false, freq);
+		filterRight.changeChebyICoefficients(freq, false, freq);
 	}
 	//==============================================================================
 	audioReadWriter.writeWavSS(stereoOut, ofile, numOfFrames, sampleRate);
