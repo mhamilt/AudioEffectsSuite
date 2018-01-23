19372448e5318667bd68209de629b9f95d395739
Tommy Rushton <8d0262a2@opayq.com>
2018-01-23 23:13:19 +0000

    Added SimpleFlanger plug-in implementation
    via JUCE. Currently output distorts for depth > 0

---
+++ Contributors/tommy/SimpleFlanger/Source/PluginEditor.cpp
@@ -0,0 +1,73 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+SimpleFlangerAudioProcessorEditor::SimpleFlangerAudioProcessorEditor (SimpleFlangerAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+
+    depthSlider.setSliderStyle (Slider::LinearVertical);
+    depthSlider.setRange(0.0, 750, 5);
+    depthSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
+    depthSlider.setPopupDisplayEnabled (false, false, this);
+    depthSlider.setTextValueSuffix (" Depth");
+    depthSlider.setValue(100);
+
+    rateSlider.setSliderStyle (Slider::LinearVertical);
+    rateSlider.setRange(0.0, 10, .02);
+    rateSlider.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
+    rateSlider.setPopupDisplayEnabled (false, false, this);
+    rateSlider.setTextValueSuffix (" Rate (Hz)");
+    rateSlider.setValue(1.0);
+
+    addAndMakeVisible(&depthSlider);
+    addAndMakeVisible(&rateSlider);
+
+    depthSlider.addListener(this);
+    rateSlider.addListener(this);
+}
+
+SimpleFlangerAudioProcessorEditor::~SimpleFlangerAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void SimpleFlangerAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setColour (Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Simple Flanger", getLocalBounds(), Justification::centred, 1);
+}
+
+void SimpleFlangerAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+    depthSlider.setBounds(40, 30, 100, getHeight() - 60);
+    rateSlider.setBounds(getWidth() - 140, 30, 100, getHeight() - 60);
+}
+
+void SimpleFlangerAudioProcessorEditor::sliderValueChanged (Slider* slider)
+{
+    double depth = depthSlider.getValue();
+    double rate = rateSlider.getValue();
+
+    processor.setParams(depth, rate);
+}
---
+++ Contributors/tommy/SimpleFlanger/Source/PluginProcessor.cpp
@@ -0,0 +1,196 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+#include "../../../../DelayEffects/SimpleDelay.hpp"
+
+//==============================================================================
+SimpleFlangerAudioProcessor::SimpleFlangerAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+SimpleFlangerAudioProcessor::~SimpleFlangerAudioProcessor()
+{
+}
+
+//==============================================================================
+const String SimpleFlangerAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SimpleFlangerAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleFlangerAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SimpleFlangerAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SimpleFlangerAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SimpleFlangerAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SimpleFlangerAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SimpleFlangerAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String SimpleFlangerAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SimpleFlangerAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void SimpleFlangerAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void SimpleFlangerAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SimpleFlangerAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SimpleFlangerAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    const int totalNumInputChannels  = getTotalNumInputChannels();
+    const int totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (int i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        float* channelData = buffer.getWritePointer (channel);
+
+        for(long sample = 0; sample < buffer.getNumSamples(); ++sample)
+        {
+            channelData[sample] = this->flanger.process(channelData[sample]);
+        }
+    }
+}
+
+//==============================================================================
+bool SimpleFlangerAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* SimpleFlangerAudioProcessor::createEditor()
+{
+    return new SimpleFlangerAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SimpleFlangerAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SimpleFlangerAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+void SimpleFlangerAudioProcessor::setParams(double depth, double rate)
+{
+    this->flanger.setEffectParams(.707, depth, rate);
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SimpleFlangerAudioProcessor();
+}
